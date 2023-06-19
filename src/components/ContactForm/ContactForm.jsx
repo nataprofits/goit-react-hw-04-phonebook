dfe54c11eb10683ed51eb640807aaeb4324306bb
Tkachenko Natalia <nataprofits@gmail.com>
2023-06-19 19:42:22 +0100

    add yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,24 @@
 import PropTypes from 'prop-types';
 import { Formik, ErrorMessage } from 'formik';
 import { Button, Input, Label, StyledForm } from './ContactForm.styled';
+import * as yup from 'yup';
 
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required(),
+  number: yup
+    .string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
 const defaultValues = {
   name: '',
   number: '',
@@ -14,17 +31,17 @@ export const ContactForm = ({ onSubmit }) => {
   };
 
   return (
-    <Formik initialValues={defaultValues} onSubmit={handleSubmitForm}>
+    <Formik 
+    initialValues={defaultValues}
+    validationSchema={schema} 
+    onSubmit={handleSubmitForm}>
       <StyledForm>
         <Label>
           Name
           <Input
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
+            id="name"/>
           <ErrorMessage name="name" component="div" />
         </Label>
         <Label>
@@ -32,10 +49,7 @@ export const ContactForm = ({ onSubmit }) => {
           <Input
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
+            id="number"/>
           <ErrorMessage name="number" component="div" />
         </Label>
         <Button type="submit">Add Contact</Button>
